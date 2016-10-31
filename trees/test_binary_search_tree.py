4dc259ec93bd0cb1a59f2058187c3f0749089e70
Ivo Lima <ivo.romario@gmail.com>
2016-10-31 12:25:32 -0300

    breaking test into smaller parts; added new tests

--- trees/test_binary_search_tree.py
+++ trees/test_binary_search_tree.py
@@ -10,33 +10,110 @@ from binary_search_tree import BinarySearchTree
 
 class TestBinarySearchTree(unittest.TestCase):
 
-    def test_binary_search_tree_add(self):
+    def setUp(self):
         # creating an empty tree
-        bst = BinarySearchTree()
-        self.assertIsNone(bst.root, 'Tree root should be empty')
+        self.bst = BinarySearchTree()
 
-        # added a single node
-        bst.add(42)
-        self.assertIsInstance(bst.root, Node, 'Obj type doesn\'t match')
-        self.assertEqual(42, bst.root.value, 'Root value doesn\'t match')
-        self.assertEqual(1, len(bst), 'Tree size doesn\'t match')
+    def add_fake_elements(self):
+        self.bst.add(3)
+        self.bst.add(2)
+        self.bst.add(1)
+        self.bst.add(4)
+        self.bst.add(5)
 
-        # added a node to the left
-        bst.add(10)
-        self.assertEqual(2, len(bst), 'Tree size doesn\'t match')
-        self.assertIsInstance(bst.root.left, Node, 'Obj type doesn\'t match')
-        self.assertEqual(10, bst.root.left.value, 'Node value doesn\'t match')
+    def test_empty_tree(self):
+        """
+        Test if tree is empty
+        """
+        self.assertTrue(self.bst.is_empty(), 'Tree should be empty')
+        self.bst.add(5)
+        self.assertFalse(self.bst.is_empty(), 'Tree shouldn\'t be empty')
 
-        # added a node to the right
-        bst.add(50)
-        self.assertEqual(3, len(bst), 'Tree size doesn\'t match')
-        self.assertIsInstance(bst.root.right, Node, 'Obj doesn\'t match')
-        self.assertEqual(50, bst.root.right.value, 'Node value doesn\'t match')
+    def test_add_node(self):
+        """
+        Adding a single node
+        """
+        self.bst.add(42)
+        self.assertIsInstance(self.bst.root, Node, 'Obj type doesn\'t match')
+        self.assertEqual(42, self.bst.root.value, 'Root value doesn\'t match')
+        self.assertEqual(1, len(self.bst), 'Tree size doesn\'t match')
 
-        # added a node with repeated value
-        bst.add(42)
-        self.assertEqual(3, len(bst), 'Repeated values should be ignored')
+    def test_add_left_node(self):
+        """
+        Adding a left node
+        """
+        self.bst.add(15)
+        self.bst.add(10)
+        self.assertEqual(2, len(self.bst), 'Tree size doesn\'t match')
+        self.assertIsInstance(self.bst.root.left, Node, 'Obj type doesn\'t match')
+        left_node = self.bst.get_left(self.bst.root)
+        self.assertEqual(10, left_node.value, 'Node value doesn\'t match')
 
+    def test_add_right_node(self):
+        """
+        Adding a right node
+        """
+        self.bst.add(15)
+        self.bst.add(50)
+        self.assertEqual(2, len(self.bst), 'Tree size doesn\'t match')
+        self.assertIsInstance(self.bst.root.right, Node, 'Obj doesn\'t match')
+        right_node = self.bst.get_right(self.bst.root)
+        self.assertEqual(50, right_node.value, 'Node value doesn\'t match')
+
+    def test_add_duplicated_value(self):
+        """
+        Adding the same value twice
+        """
+        self.bst.add(42)
+        self.bst.add(42)
+        self.assertEqual(1, len(self.bst), 'Repeated values should be ignored')
+
+    def test_traversal_invalid_method(self):
+        """
+        Traversins tree using an invalid method
+        """
+        with self.assertRaises(ValueError):
+            self.bst.traversal(method='invalid')
+
+    def test_traversal_inorder(self):
+        """
+        Traversing tree using inorder method
+        """
+        self.add_fake_elements()
+        result = self.bst.traversal(method='inorder')
+        self.assertIsInstance(result, list, 'Return must be a list instance')
+        self.assertListEqual([1, 2, 3, 4, 5], result,'Result doesn\'t match')
+
+    def test_traversal_preorder(self):
+        """
+        Traversing tree using preorder method
+        """
+        self.add_fake_elements()
+        result = self.bst.traversal(method='preorder')
+        self.assertIsInstance(result, list, 'Return must be a list instance')
+        self.assertListEqual([3, 2, 1, 4, 5], result,'Result doesn\'t match')
+
+    def test_traversal_postorder(self):
+        """
+        Traversing tree using postorder method
+        """
+        self.add_fake_elements()
+        result = self.bst.traversal(method='postorder')
+        self.assertIsInstance(result, list, 'Return must be a list instance')
+        self.assertListEqual([1, 2, 5, 4, 3], result,'Result doesn\'t match')
+
+    def test_traversal_empty_tree(self):
+        """
+        Traversing an empty tree
+        """
+        methods = ['inorder', 'preorder', 'postorder']
+        for method in methods:
+            result = self.bst.traversal(method=method)
+            self.assertIsInstance(result, list, 'Return must be a list instance')
+            self.assertListEqual([], result,'Result should be empty list')
+
+    def test_get_left(self):
+        self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main()
